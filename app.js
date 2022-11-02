8b0fa41069eae2c4ddf2303297cf79cf854356cf
Amruthavarshini27 <Amruthavarshini27@gmail.com>
2022-11-02 14:28:07 -0500

    grid selector and vehicle

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var vehicleRouter = require('./routes/vehicle');
+var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -20,6 +23,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/vehicle', vehicleRouter);
+app.use('/selector', selectorRouter);
+app.use('/gridbuild', gridbuildRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
